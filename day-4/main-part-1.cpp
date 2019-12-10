aab89f99c78a7e1199bc0fe251ebcd6a5024021c
Ellinor <58392209+helloellinor@users.noreply.github.com>
2019-12-09 22:37:11 -0500

    "Added day 4"

---
+++ day-4/main-part-1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	int answers = 0;
+	for (int i = 138307; i <= 654504; ++i) {
+		std::string password = std::to_string(i);
+		for (int j = 5; j >= 0; --j)
+			if (password[j] < password[j-1])
+				for (int k = j; k <= 5; ++k)
+			 		password[k] = password[j-1];
+		int consecutive = 0;
+		for (int j = 5; j >= 0; --j)
+			if (password[j] == password[j-1])
+				++consecutive;
+		if (consecutive > 0) {
+			i = std::stoi(password);
+			if (i >= 654504)
+				break;
+			++answers;
+		}
+		
+	}
+	std::cout << "Number of unique passwords: " << answers << '\n';
+	return 0;
+}	
